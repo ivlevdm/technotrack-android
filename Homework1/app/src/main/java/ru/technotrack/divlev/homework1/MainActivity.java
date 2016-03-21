9c696957a5f0ce1dc4a4391b4758f2bd1989f1d3
ivlevdm <ivlev.dm@frtk.ru>
2016-03-21 21:13:57 +0300

    Add second activity

--- Homework1/app/src/main/java/ru/technotrack/divlev/homework1/MainActivity.java
+++ Homework1/app/src/main/java/ru/technotrack/divlev/homework1/MainActivity.java
@@ -2,12 +2,21 @@ package ru.technotrack.divlev.homework1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
     }
 }
