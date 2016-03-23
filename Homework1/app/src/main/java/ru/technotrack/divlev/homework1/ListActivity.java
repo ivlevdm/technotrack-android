45dc089eb006cb2088bb918cd04ff9672f966e2f
ivlevdm <ivlev.dm@frtk.ru>
2016-03-23 08:20:20 +0300

    Add adapter

--- Homework1/app/src/main/java/ru/technotrack/divlev/homework1/ListActivity.java
+++ Homework1/app/src/main/java/ru/technotrack/divlev/homework1/ListActivity.java
@@ -1,11 +1,15 @@
 package ru.technotrack.divlev.homework1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
+    private ListAdapter listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,36 @@ public class ListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list);
 
         ListView listView = (ListView)findViewById(R.id.listView);
+        listAdapter = new ListAdapter(this);
+        listView.setAdapter(listAdapter);
+    }
+
+    private class ListAdapter extends BaseAdapter {
+        private final Context context;
+
+
+        public ListAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return 1000000;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+        }
     }
 }
